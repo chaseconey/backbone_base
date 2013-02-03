d45116e07e5ac6dba70da8b296a3172e1c938a75
Chase Coney <chasiepoo@gmail.com>
2013-02-02 23:28:24 -0600

    Code cleanup and changed all vars to be name spaced properly. Following best practices now

--- js/main.js
+++ js/main.js
@@ -14,10 +14,25 @@
 // 	return this.name + " is doing work";
 // };
 
+(function(){
+
+//global namespacing
+window.App = {
+	Models: {},
+	Views: {},
+	Collections: {}
+};
+
+//This is a template helper that will make the template code a bit cleaner
+window.template = function(id) {
+	return $("#" + id).html();
+};
+
+
 
 // This object is created in the same way as before in the console
 // person1 = new Person();
-var Person = Backbone.Model.extend({
+App.Models.Person = Backbone.Model.extend({
 	//These are the attributes that come with the object
 	//by default on instantiation
 	defaults: {
@@ -43,12 +58,12 @@ var Person = Backbone.Model.extend({
 	}
 });
 
-var PeopleCollection = Backbone.Collection.extend({
-	model: Person
+App.Collections.People = Backbone.Collection.extend({
+	model: App.Models.Person
 });
 
 //We now need a View for the collection
-var PeopleView = Backbone.View.extend({
+App.Views.People = Backbone.View.extend({
 	tagName: "ul",
 
 	initialize: function() {
@@ -58,7 +73,7 @@ var PeopleView = Backbone.View.extend({
 	render: function() {
 		//Here we can pass this as a second parameter to keep the proper scope
 		this.collection.each(function(person){
-			var personView = new PersonView({ model: person });
+			var personView = new App.Views.Person({ model: person });
 			this.$el.append( personView.render().el );
 		}, this);
 
@@ -66,13 +81,13 @@ var PeopleView = Backbone.View.extend({
 	}
 });
 
-var PersonView = Backbone.View.extend({
+App.Views.Person = Backbone.View.extend({
 	tagName: "li",
 	//className: "person",
 	//id: "person-id"
 
 	//Separating it out a little now, point to the script with selector
-	template: _.template( $("#personTemplate").html() ),
+	template: _.template( template("personTemplate") ),
 
 	render: function() {
 		this.$el.html( this.template(this.model.toJSON()) );
@@ -81,14 +96,15 @@ var PersonView = Backbone.View.extend({
 });
 
 //Sample creation of Model and View
-var peopleCollection = new PeopleCollection(
+var peopleCollection = new App.Collections.People(
 	[
 		{name: "Chase Coney", age: 23, job: "Software Developer"},
 		{name: "Sarah Daniels", age: 22, job: "Marketing Manager"},
 		{name: "Murphy Judge Joe Brown", age: 65, job: "Garbage Man"}
 	]
 );
-var peopleView = new PeopleView({ collection: peopleCollection });
+var peopleView = new App.Views.People({ collection: peopleCollection });
 
 $(document.body).append(peopleView.render().el);
 
+})();
